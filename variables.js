bcd6742b0cc7a2e17da8c7e727573e99c30c4f45
Jeff Pickelman <pickelman@gmail.com>
2015-10-28 13:54:05 -0700

    Update payout structure to 50-30-20

--- variables.js
+++ variables.js
@@ -1,17 +1,12 @@
 
 'use strict';
 
-export const buyIn = 20;
+export const buyIn = 20; 
+
 export const startingSeconds = 15 * 60;
+
 export const smallBlinds = [ 100, 200, 300, 400, 500, 800, 1000, 1500, 2000, 3000, 5000];
+
 export function payouts( pot ) {
-    if ( pot <= 80 ) {
-        return [ pot, 0, 0 ];
-    } else if ( pot <= 140 ) {
-        return [ pot - 30, 30, 0 ];
-    } else if ( pot <= 180 ) {
-        return [ pot - 60, 40, 20 ];
-    } else {
-        return [ pot - 80, 60, 20 ];
-    }
-};
+    return [ pot * 0.5, pot * 0.3, pot * 0.2 ];
+}
